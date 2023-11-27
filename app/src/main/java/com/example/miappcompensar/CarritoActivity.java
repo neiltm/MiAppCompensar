2b9090a13bde47eab5568b0ac5d6eec7c05f7ffc
neiltm <32169812+neiltm@users.noreply.github.com>
2023-11-27 00:31:43 -0500

    README.md

---
+++ app/src/main/java/com/example/miappcompensar/CarritoActivity.java
@@ -0,0 +1,14 @@
+package com.example.miappcompensar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CarritoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrito);
+    }
+}
